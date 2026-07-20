01a7542786bb72d6b1fbe4f8485ac1263d48704d
yaakovizel <z7685727@gmail.com>
2026-07-20 07:31:57 +0300

    added shtdown function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import { router as operatorRouter } from "./routes/operators_routes.js";
 import { router as incidentRouter } from "./routes/incidents_routes.js";
 import { errorHandler } from "./middlewares/error_middlware.js";
+import { pool } from "./db/database.js";
 
 const app = express();
 
@@ -10,7 +11,30 @@ app.use(express.json());
 
 app.use("/operators", operatorRouter);
 app.use("/incidents", incidentRouter);
+app.use((req, res) =>
+  res.status(404).json({
+    success: false,
+    message: `Route ${req.url} not exists for method ${req.method}`,
+  }),
+);
 app.use(errorHandler);
-app.listen(process.env.PORT, () =>
+const server = app.listen(process.env.PORT, () =>
   console.log(`Server is listening in port ${process.env.PORT}`),
 );
+
+async function shutdown() {
+  try {
+    await pool.end();
+    console.log("Mysql connection pool closed");
+    server.close(() => {
+      console.log("Express server closed");
+      process.exit(0);
+    });
+  } catch (err) {
+    console.log("Error during shutdown");
+    process.exit(1);
+  }
+}
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
